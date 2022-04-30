c4a0babde15aa836227618e1fcc73398dfdccb97
yedindin <21900160@handong.edu>
2022-04-30 23:14:03 +0900

    add : 계정관리 추

--- src/com/handong/oodp/HospitalProgram.java
+++ src/com/handong/oodp/HospitalProgram.java
@@ -5,14 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 
 public class HospitalProgram {
@@ -25,7 +22,7 @@ public class HospitalProgram {
 		int num1, num2 = 0;
 		String id, pw;
 		String position = "fail";
-		BufferedReader br = Files.newBufferedReader(Paths.get("./data/userdata.csv"), Charset.forName("EUC-KR"));
+		BufferedReader br = Files.newBufferedReader(Paths.get("./data/userdata.csv"), Charset.forName("UTF-8"));
 
 		List<List<String>> list = new ArrayList<List<String>>();
 		BufferedReader bufferedReader = null;
@@ -58,141 +55,190 @@ public class HospitalProgram {
 		EmploymentManagement emplmanage = new EmploymentManagement(list);
 
 		/*
-		 * for (List<String> item : list) { System.out.println(item.get(0) + "and" +
-		 * item.get(1) + item.get(2) + item.get(3) + item.get(4) ); }
+		 * 리스트 출력 for (List<String> item : list) { System.out.println(item.get(0) +
+		 * "and" + item.get(1) + item.get(2) + item.get(3) + item.get(4) ); }
 		 */
-		while (position == "fail") {
-			System.out.println("ID를 입력하세요.");
-			id = sc.next();
-			System.out.println("패스워드를 입력하세요.");
-			pw = sc.next();
-			position = login.login(id, pw, list); // position 에 따라 다른 기능 구현
-			if (position == "fail") {
-				System.out.println("아이디나 비밀번호를 잘못입력하셨습니다.");
-				System.out.println("다시 로그인해 주세요.");
-			}
-		}
-		if (position.equals("manager")) {
+		while(true) {
 
-			while (run) {
-				System.out.println("**** 병원 관리 프로그램 ****");
-				System.out.println("1. 고용관리");
-				System.out.println("2. 계정관리");
-				System.out.println("0. 종료\n\n");
+			System.out.println("**** 병원 관리 프로그램 ****");
+			System.out.println("1. 로그인");
+			System.out.println("2. 회원등록");
+			System.out.println("0. 종료\n\n");
+			System.out.print("원하는 메뉴의 번호를 입력하세요 : ");
+			
+			int state = sc.nextInt();
+			if (state == 1) { //로그인
+				while (position == "fail") {
+					System.out.println("ID를 입력하세요.");
+					id = sc.next();
+					System.out.println("패스워드를 입력하세요.");
+					pw = sc.next();
+					position = login.login(id, pw, list); // position 에 따라 다른 기능 구현
+					if (position == "fail") {
+						System.out.println("아이디나 비밀번호를 잘못입력하셨습니다.");
+						System.out.println("다시 로그인해 주세요.");
+					}
+				}
+				if (position.equals("manager")) {
 
-				System.out.print("원하는 메뉴의 번호를 입력하세요 : ");
-				num1 = sc.nextInt();
-				if (num1 == 0) { // 종료
-					run = false;
-					System.out.println("병원 관리 프로그램을 종료합니다.");
-				} else if (num1 == 1) {// 고용관리
-					while (true) {
-						System.out.println("\n\n**** 고용 관리 ****");
-						System.out.println("1. 의사/간호사 추가");
-						System.out.println("2. 의사/간호사 삭제");
-						System.out.println("0. 이전으로 돌아가기\n\n");
+					while (run) {
+						System.out.println("**** 병원 관리 프로그램 ****");
+						System.out.println("1. 고용관리");
+						System.out.println("2. 계정관리");
+						System.out.println("0. 종료\n\n");
+
 						System.out.print("원하는 메뉴의 번호를 입력하세요 : ");
-						num2 = sc.nextInt();
+						num1 = sc.nextInt();
+						if (num1 == 0) { // 종료
+							run = false;
+							System.out.println("병원 관리 프로그램을 종료합니다.");
+						} else if (num1 == 1) {// 고용관리
+							while (true) {
+								System.out.println("\n\n**** 고용 관리 ****");
+								System.out.println("1. 의사/간호사 추가");
+								System.out.println("2. 의사/간호사 삭제");
+								System.out.println("0. 이전으로 돌아가기\n\n");
+								System.out.print("원하는 메뉴의 번호를 입력하세요 : ");
+								num2 = sc.nextInt();
 
-						if (num2 == 1) {
-							System.out.println("\n\n**** 추가하기 ****");
-							System.out.println("1. 의사 추가");
-							System.out.println("2. 간호사 추가");
-							System.out.println("0. 처음으로 돌아가기\n\n");
-							System.out.print("원하는 메뉴의 번호를 입력하세요 : ");
-							int num = sc.nextInt();
-							if (num == 1) {
-								User user = new User(list);
-								System.out.println("이름을 입력해주세요.");
-								user.setName(sc.next());
-								System.out.println("나이를 입력해주세요.");
-								user.setAge(sc.next());
-								System.out.println("부서를 입력해주세요.");
-								user.setDepartment(sc.next());
-								user.setPosition("Doctor");
-								list = emplmanage.addDoctor(user);
+								if (num2 == 1) {
+									System.out.println("\n\n**** 추가하기 ****");
+									System.out.println("1. 의사 추가");
+									System.out.println("2. 간호사 추가");
+									System.out.println("0. 처음으로 돌아가기\n\n");
+									System.out.print("원하는 메뉴의 번호를 입력하세요 : ");
+									int num = sc.nextInt();
+									if (num == 1) {
+										User user = new User(list);
+										System.out.println("이름을 입력해주세요.");
+										user.setName(sc.next());
+										System.out.println("나이를 입력해주세요.");
+										user.setAge(sc.next());
+										System.out.println("부서를 입력해주세요.");
+										user.setDepartment(sc.next());
+										user.setPosition("Doctor");
+										list = emplmanage.addDoctor(user);
 
-								try {
+										try {
 
-									/*
-									 * for(int i = 0; i < list.size(); i++) { StringBuffer data = new
-									 * StringBuffer(); Charset.forName("UTF-8");
-									 * 
-									 * 
-									 * for(int k = 0; k < 6; k++) { System.out.println(list.get(i).get(k));
-									 * data.append(list.get(i).get(k) + ","); } data.append('\n');
-									 * 
-									 * FileOutputStream outputStream = new FileOutputStream("./data/userdata.csv");
-									 * outputStream.write(data.toString().getBytes()); outputStream.close(); }
-									 */
-									StringBuffer data = new StringBuffer();
-									Charset.forName("EUC-KR");
-									for (List<String> item : list) {
+											StringBuffer data = new StringBuffer();
+											Charset.forName("UTF-8");
+											for (List<String> item : list) {
 
-										data.append(item.get(0) + "," + item.get(1) + "," + item.get(2) + ","
-												+ item.get(3) + "," + item.get(4) + "," + item.get(5) + "\n");
-									}
-									FileOutputStream outputStream = new FileOutputStream("./data/userdata.csv");
-									outputStream.write(data.toString().getBytes());
-									outputStream.close();
+												data.append(item.get(0) + "," + item.get(1) + "," + item.get(2) + ","
+														+ item.get(3) + "," + item.get(4) + "," + item.get(5) + "\n");
+											}
+											FileOutputStream outputStream = new FileOutputStream("./data/userdata.csv");
+											outputStream.write(data.toString().getBytes());
+											outputStream.close();
 
-									System.out.println("저장완료 : ");
+											System.out.println("저장완료");
 
-								} catch (FileNotFoundException e) {
-									e.printStackTrace();
-								} catch (IOException e) {
-									e.printStackTrace();
-								} finally {
-									try {
-										if (br != null) {
-											br.close();
+										} catch (FileNotFoundException e) {
+											e.printStackTrace();
+										} catch (IOException e) {
+											e.printStackTrace();
+										} finally {
+											try {
+												if (br != null) {
+													br.close();
+												}
+											} catch (IOException e) {
+												e.printStackTrace();
+
+											}
 										}
-									} catch (IOException e) {
-										e.printStackTrace();
 
+									} else if (num == 2) {
+										User user = new User(list);
+										System.out.println("이름을 입력해주세요.");
+										user.setName(sc.next());
+										System.out.println("나이를 입력해주세요.");
+										user.setAge(sc.next());
+										System.out.println("부서를 입력해주세요.");
+										user.setDepartment(sc.next());
+										user.setPosition("Doctor");
+										list = emplmanage.addNurse(user);
+
+										try {
+											StringBuffer data = new StringBuffer();
+											Charset.forName("EUC-KR");
+											for (List<String> item : list) {
+
+												data.append(item.get(0) + "," + item.get(1) + "," + item.get(2) + ","
+														+ item.get(3) + "," + item.get(4) + "," + item.get(5) + "\n");
+											}
+											FileOutputStream outputStream = new FileOutputStream("./data/userdata.csv");
+											outputStream.write(data.toString().getBytes());
+											outputStream.close();
+
+											System.out.println("저장완료");
+
+										} catch (FileNotFoundException e) {
+											e.printStackTrace();
+										} catch (IOException e) {
+											e.printStackTrace();
+										} finally {
+											try {
+												if (br != null) {
+													br.close();
+												}
+											} catch (IOException e) {
+												e.printStackTrace();
+
+											}
+										}
+									} else {
+										break;
 									}
+								} else if (num2 == 2) {
+									System.out.println("\n\n**** 삭제하기 ****");
+									System.out.println("1. 의사 삭제");
+									System.out.println("2. 간호사 삭제");
+									System.out.println("0. 처음으로 돌아가기\n\n");
+									System.out.print("원하는 메뉴의 번호를 입력하세요 : ");
+									int num = sc.nextInt();
+									if (num == 1) {
+										User user = new User(list);
+										list = emplmanage.deleteDoctor(user);
+									} else if (num == 2) {
+										User user = new User(list);
+										list = emplmanage.deleteNurse(user);
+									} else {
+										break;
+									}
+								} else {
+									break;
 								}
+							}
+						} else if (num1 == 2) {// 계정관리
 
-							} else if (num == 2) {
-								User user = new User(list);
-								list = emplmanage.addNurse(user);
-							} else {
-								break;
-							}
-						} else if (num2 == 2) {
-							System.out.println("\n\n**** 삭제하기 ****");
-							System.out.println("1. 의사 삭제");
-							System.out.println("2. 간호사 삭제");
-							System.out.println("0. 처음으로 돌아가기\n\n");
-							System.out.print("원하는 메뉴의 번호를 입력하세요 : ");
-							int num = sc.nextInt();
-							if (num == 1) {
-								User user = new User(list);
-								list = emplmanage.deleteDoctor(user);
-							} else if (num == 2) {
-								User user = new User(list);
-								list = emplmanage.deleteNurse(user);
-							} else {
-								break;
-							}
+						} else if (num1 == 3) {// 로그인
+
 						} else {
-							break;
+							System.out.println("잘못된 입력입니다. 다시 입력해주세요.\n\n");
+							continue;
 						}
 					}
-				} else if (num1 == 2) {// 계정관리
 
-				} else if (num1 == 3) {// 로그인
+				} else
 
-				} else {
-					System.out.println("잘못된 입력입니다. 다시 입력해주세요.\n\n");
-					continue;
+				{
+					System.out.println("매니저아님");
 				}
 			}
-
-		} else
-
-		{
-			System.out.println("매니저아님");
+			else if(state==2) {
+				System.out.println("성함을 입력해 주세요.");
+				User user = new User(list);
+				list = user.addUser(sc.next());
+				
+			}
+			else if(state==0) {
+				break;
+			}else {
+				System.out.println("잘못된 입력입니다. 다시 입력해주세요.\n\n");
+				continue;
+			}
 		}
 	}
 
--- src/com/handong/oodp/User.java
+++ src/com/handong/oodp/User.java
@@ -2,6 +2,7 @@ package com.handong.oodp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class User {
 
@@ -12,24 +13,41 @@ public class User {
 	private String position;
 	private String age;
 	private List<List<String>> list;
-	
-	
-	public User (List<List<String>> list) {
+
+	public User(List<List<String>> list) {
 		this.list = list;
 	}
 
-	public List<List<String>> addUser(User user) {
-		List<String> data = new ArrayList<>();
-		data.add(0,name);
-		data.add(3,department);
-		data.add(4,position);
-		data.add(5,age);
-		list.add(data);
-		for (List<String> item : list) { System.out.println(item.get(0) + "and" +
-				 item.get(1) + item.get(2) + item.get(3) + item.get(4) + item.get(5) ); }
-		System.out.println("user 추가 완료");
+	public List<List<String>> addUser(String name) {
+		Scanner sc = new Scanner(System.in);
+
+		for (List<String> item : list) {
+			if (name.equals(item.get(0))&&item.get(1).equals("")) {
+				System.out.println("사용할 ID를 입력하세요.");
+				this.ID = sc.next();
+				System.out.println("사용할 패스워드를 입력하세요.");
+				this.PW = sc.next();
+				System.out.println("회원등록이 완료되었습니다.");
+				item.set(1,this.ID);
+				item.set(2, this.PW);
+				return list;
+			}
+		}
+		System.out.println("등록 대상이 아닙니다.");
 		return list;
+s
+//		List<String> data = new ArrayList<>();
+//		data.add(0,name);
+//		data.add(3,department);
+//		data.add(4,position);
+//		data.add(5,age);
+//		list.add(data);
+//		for (List<String> item : list) { System.out.println(item.get(0) + "and" +
+//				 item.get(1) + item.get(2) + item.get(3) + item.get(4) + item.get(5) ); }
+//		System.out.println("user 추가 완료");
+//		return list;
 	}
+
 //	public void editUser(User user) {
 //		System.out.println("user 수정 완료");
 //	}
@@ -38,42 +56,53 @@ public class User {
 		return list;
 
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public String getID() {
 		return ID;
 	}
+
 	public String getPW() {
 		return PW;
 	}
+
 	public String getDepartment() {
 		return department;
 	}
+
 	public String getPosition() {
 		return position;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setID(String iD) {
 		ID = iD;
 	}
+
 	public void setPW(String pW) {
 		PW = pW;
 	}
+
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+
 	public void setPosition(String position) {
 		this.position = position;
 	}
+
 	public String getAge() {
 		return age;
 	}
+
 	public void setAge(String string) {
 		this.age = string;
 	}
-	
-	
+
 }
