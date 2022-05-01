6752a61f95175dc63306052c85c796ebd1d77a42
s21600651 <21600651@handong.edu>
2022-05-01 15:00:38 +0900

    add delete, menu edit

--- src/com/handong/oodp/Work.java
+++ src/com/handong/oodp/Work.java
@@ -176,6 +176,55 @@ public class Work {
 			System.out.println("원하는 삭제 시간을 입력하세요(예시. 월1).");
 			String time = sc.next();
 			System.out.println(time);
+
+			if (time.charAt(0)=='월')
+				i=0;
+			else if (time.charAt(0)=='화')
+				i=1;
+			else if (time.charAt(0)=='수')
+				i=2;
+			else if (time.charAt(0)=='목')
+				i=3;
+			else if (time.charAt(0)=='금')
+				i=4;
+			else if (time.charAt(0)=='토')
+				i=5;
+			else if (time.charAt(0)=='일')
+				i=6;
+			if (time.charAt(1)=='1')
+				j=0;
+			else if (time.charAt(1)=='2')
+				j=1;
+			else if (time.charAt(1)=='3')
+				j=2;
+			
+	         List<List<String>> week = schedule.get(j);
+	         List<String> temp = schedule.get(j).get(i);
+	         String[] people_list, new_list;
+	         people_list = temp.toArray(new String[temp.size()]);
+	         new_list = new String[temp.size()-1];
+	         for(int i1=0; i1<temp.size(); i1++) {
+	        	 if(crew_name.equals(people_list[i1])) {
+	        		 if(i1==temp.size()-1) {
+	        			 for(int i2=0; i2<temp.size()-1; i2++) {
+	    		        	 new_list[i2] = people_list[i2];
+	        			 }
+	        		 }
+	        		 else {
+	        			 for(int i2=0; i2<temp.size()-1; i2++) {
+	        				 if(i2<=i1) {
+	        					 new_list[i2] = people_list[i2];
+	        				 }
+	        				 else {
+	        					 new_list[i2] = people_list[i2-1];
+	        				 }
+	        			 }
+	        		 }
+	        	 }
+	        	
+	         }
+	         week.set(i, Arrays.asList(new_list));
+	         schedule.set(j, week);
 			// 삭제**
 			// save
 			// update
@@ -235,10 +284,38 @@ public class Work {
 					// save
 				}
 		}
-		else {
+		else {			
 			System.out.println("원하는 추가 시간을 입력하세요(예시. 월1).");
 			String time = sc.next();
 			System.out.println(time);
+			if (time.charAt(0)=='월')
+				i=0;
+			else if (time.charAt(0)=='화')
+				i=1;
+			else if (time.charAt(0)=='수')
+				i=2;
+			else if (time.charAt(0)=='목')
+				i=3;
+			else if (time.charAt(0)=='금')
+				i=4;
+			else if (time.charAt(0)=='토')
+				i=5;
+			else if (time.charAt(0)=='일')
+				i=6;
+			if (time.charAt(1)=='1')
+				j=0;
+			else if (time.charAt(1)=='2')
+				j=1;
+			else if (time.charAt(1)=='3')
+				j=2;
+			
+	         List<List<String>> week = schedule.get(j);
+	         List<String> temp = schedule.get(j).get(i);
+	         String[] people_list;
+	         people_list = temp.toArray(new String[temp.size()+1]);
+	         people_list[temp.size()]=user;
+	         week.set(i, Arrays.asList(people_list));
+	         schedule.set(j, week);
 			// 추가**
 			// save
 			// update
